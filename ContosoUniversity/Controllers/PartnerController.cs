8ca2f4b1736a5c2830f5529b7d006f1ca31574a9
dellhoffman <dellanhoffman@gmail.com>
2017-07-16 16:34:16 -0300

    Adicionar arquivos de projeto.

---
+++ ContosoUniversity/Controllers/PartnerController.cs
@@ -0,0 +1,144 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers
+{
+    public class PartnerController : Controller
+    {
+        private SchoolContext db = new SchoolContext();
+
+        // GET: Partner
+        public ActionResult Index()
+        {
+            return View(db.Partners.ToList());
+        }
+
+        // GET: Partner/Details/5
+        public ActionResult Details(int id)
+        {
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Partner partner = db.Partners.Find(id);
+            if (partner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partner);
+        }
+
+        // GET: Partner/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Partner/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name")]Partner partner)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Partners.Add(partner);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (RetryLimitExceededException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.)
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+            }
+            return View(partner);
+        }
+
+        // GET: Partner/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Partner partner = db.Partners.Find(id);
+            if (partner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partner);
+        }
+
+        // POST: Partner/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id)
+        {
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var partnerUpdate = db.Partners.Find(id);
+            if (TryUpdateModel(partnerUpdate, "", new string[] { "Name" }))
+            {
+                try
+                {
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                catch (RetryLimitExceededException /* dex */)
+                {
+                    //Log the error (uncomment dex variable name and add a line here to write a log.
+                    ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists, see your system administrator.");
+                }
+            }
+
+            return View(partnerUpdate);
+        }
+
+        // GET: Partner/Delete/5
+        public ActionResult Delete(int id)
+        {
+            if (id == 0)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Partner partner = db.Partners.Find(id);
+            if (partner == null)
+            {
+                return HttpNotFound();
+            }
+            return View(partner);
+        }
+
+        // POST: Course/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Partner partner = db.Partners.Find(id);
+            db.Partners.Remove(partner);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ContosoUniversity/Models/Partner.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Partner
+    {
+        public int PartnerID { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Número máximo de caracters atingido!")]
+        public string Name { get; set; }
+    }
+}
